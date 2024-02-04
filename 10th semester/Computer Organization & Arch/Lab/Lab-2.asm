787f1499154fe294b9f766a20aa7fc69aa5cebe2
Nokibul Arfin Siam <111479069+nokibularfinsiam@users.noreply.github.com>
2024-02-04 15:15:47 +0600

    Create Lab-2.asm

---
+++ 10th semester/Computer Organization & Arch/Lab/Lab-2.asm
@@ -0,0 +1,53 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+a DB 'Enter A number: $'
+NEWL DB 10, 13, '$'
+
+.code
+Main proc
+    
+    mov ax, @data
+    mov ds, ax
+    
+    mov ah, 9h
+    lea dx, a
+    int 21h
+    
+    mov ah, 1
+    int 21h
+    mov bh, al
+    
+    mov ah, 9
+    lea dx, NEWL
+    int 21h
+    
+    mov ah, 9h
+    lea dx, a
+    int 21h
+    
+    mov ah, 1
+    int 21h
+    mov ch, al
+    
+    mov ah, 9
+    lea dx, NEWL
+    int 21h
+    
+    mov ah, 2
+    mov dl, bh
+    int 21h
+    
+    mov ah, 9
+    lea dx, NEWL
+    int 21h
+    
+    mov ah, 2
+    mov dl, ch
+    int 21h
+    
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
